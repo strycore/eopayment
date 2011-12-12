d21df94b11a0513b9ec7c63a6bce59fd8e612e6f
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2011-12-12 17:05:02 +0100

    [spplus] add way to test from command line

--- eopayment/spplus.py
+++ eopayment/spplus.py
@@ -131,9 +131,12 @@ if __name__ == '__main__':
     ntkey = '58 6d fc 9c 34 91 9b 86 3f fd 64 63 c9 13 4a 26 ba 29 74 1e c7 e9 80 79'
     payment = Payment({'cle': ntkey, 'siret': '00000000000001-01'})
     print payment.request(10)
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
         print sign_url_paiement(ntkey, sys.argv[1])
         print sign_ntkey_query(ntkey, sys.argv[1])
+    elif len(sys.argv) > 2:
+        print sign_url_paiement(sys.argv[1], sys.argv[2])
+        print sign_ntkey_query(sys.argv[1], sys.argv[2])
     else:
         tests = [('x=coin', 'c04f8266d6ae3ce37551cce996c751be4a95d10a'),
                  ('x=coin&y=toto', 'ef008e02f8dbf5e70e83da416b0b3a345db203de')]
