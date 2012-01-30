e5d240bbfdc1637d2a22b736134b93fc0ac12e75
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-01-30 15:45:20 +0100

    declare pycrypto dependency, add long description

--- setup.py
+++ setup.py
@@ -12,9 +12,16 @@ distutils.core.setup(name='eopayment',
         version=eopayment.__version__,
         license='GPLv3 or later',
         description='Common API to use all French online payment credit card processing services',
+        long_description=
+            "eopayment is a Python module to interface with French's bank credit card\n"
+            "online payment services. Supported services are ATOS/SIP, SystemPay, and\n"
+            "SPPLUS.",
         url='http://dev.entrouvert.org/projects/eopayment/',
         author="Entr'ouvert",
         author_email="info@entrouvert.com",
         maintainer="Benjamin Dauvergne",
         maintainer_email="bdauvergne@entrouvert.com",
-        packages=['eopayment'])
+        packages=['eopayment'],
+        requires=[
+            'pycrypto (>= 2.5)'
+        ])
