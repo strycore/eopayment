e9e3f0cc633b56254dee10af558f160b19f310ce
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-02-17 16:30:01 +0100

    Add a common response object for all payment modules

--- eopayment/common.py
+++ eopayment/common.py
@@ -13,6 +13,36 @@ RANDOM = random.SystemRandom()
 URL = 1
 HTML = 2
 
+class PaymentResponse(object):
+    '''Holds a generic view on the result of payment transaction response.
+
+       result -- holds the declarative result of the transaction, does not use
+       it to validate the payment in your backoffice, it's just for informing
+       the user that all is well.
+       signed_result -- holds the signed result of the transaction, when it is
+       not None, it contains the result of the transaction as asserted by the
+       bank with an electronic signature.
+       bank_data -- a dictionnary containing some data depending on the bank,
+       you have to log it for audit purpose.
+       return_content -- when handling a response in a callback endpoint, i.e.
+       a response transmitted directly from the bank to the merchant website,
+       you usually have to confirm good reception of the message by returning a
+       properly formatted response, this is it.
+       bank_error_status -- if result is False, it contains the reason
+       transaction_id -- the id assigned by the bank to this transaction, it
+       could be the one sent by the merchant in the request, but it is usually
+       an identifier internal to the bank.
+    '''
+
+    def __init__(self, result=None, signed_result=None, bank_data=dict(),
+            return_content=None, bank_error_status='', transaction_id=''):
+        self.result = result
+        self.signed_result = signed_result
+        self.bank_data = bank_data
+        self.return_content = return_content
+        self.bank_error_status = bank_error_status
+        self.transaction_id = transaction_id
+
 class PaymentCommon(object):
     PATH = '/tmp'
     BANK_ID = '__bank_id'
